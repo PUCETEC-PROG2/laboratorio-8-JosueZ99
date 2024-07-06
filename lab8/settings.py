fad1b99f092d60b129f6aa0fb579be2f0223ec3d
Josue Zambrano <jjzambranoz@puce.edu.ec>
2024-07-06 10:10:23 -0500

    Avances 6 de julio

--- lab8/settings.py
+++ lab8/settings.py
@@ -77,8 +77,16 @@ WSGI_APPLICATION = 'lab8.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'pokedex',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    # }
 }
 
 
