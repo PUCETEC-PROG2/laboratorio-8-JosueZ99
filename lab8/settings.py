6237376b79782baa4800567f4ff26ac730b426ea
josuez99 <josuez99@UbuntuVM.myguest.virtualbox.org>
2024-07-26 12:21:19 -0500

    avances 26 julio 2

--- lab8/settings.py
+++ lab8/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 'default':{
 
     'ENGINE':'django.db.backends.postgresql',
-    'NAME':'pokedex',
+    'NAME':'pokedex_josuez',
     'HOST':'localhost',
     'PORT':'5432',
     'USER':'postgres',
