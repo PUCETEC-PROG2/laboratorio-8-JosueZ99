625366fc07ae080d25cd421e65dab8ba239bb1fa
JosueZ99 <152650329+JosueZ99@users.noreply.github.com>
2024-07-26 12:06:03 -0500

    avances 26 julio

---
+++ laboratorio-8-JosueZ99/pokedex/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.template import loader
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+
+from .models import Pokemon
+from pokedex.forms import PokemonForm
+
+def index(request):
+    pokemons = Pokemon.objects.order_by('type')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'pokemons': pokemons}, request))
+
+def pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(pk = pokemon_id)
+    template = loader.get_template('display_pokemon.html')
+    context = {
+        'pokemon': pokemon
+    }
+    return HttpResponse(template.render(context, request))
+
+def add_pokemon(request):
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm()
+
+    return render(request, 'add_pokemon.html', {'form':form})
+
+
+class CustomLoginView(LoginView):
+    template_name="login.html"
